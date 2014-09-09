97189f8e85cb3548eb0b8d0d91e4452f9df8a179
yunhua.xiao <yunhua.xiao@dianping.com>
2014-09-09 20:59:39 +0800

    init

--- service/api.js
+++ service/api.js
@@ -1,4 +1,27 @@
 var API = require('../model/api');
+var Param = require('../model/param');
+
+var creatParam = function (params) {
+  if (Object.prototype.toString.call(params) !== '[object Array]') {
+    return;
+  }
+
+  var rs = [];
+
+  params.forEach(function (param) {
+    if (param.values && param.values.length) {
+      param.values = createParam(param.values);
+    }
+
+    Param.create(param, function (err, res) {
+      if (res) {
+        rs.push(res._id);
+      }
+    });
+  });
+
+  return rs;
+}
 
 module.exports = {
   find: function (data, callback) {
@@ -10,6 +33,8 @@ module.exports = {
   },
 
   create: function (data, callback) {
+    data.data = createParam(data.data);
+
     API.create(data, callback);
   }
 }
